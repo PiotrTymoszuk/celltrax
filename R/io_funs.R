57b8a8f342d9b3affcd860b8f0875c034ed91af4
PiotrTymoszuk <80723424+PiotrTymoszuk@users.noreply.github.com>
2022-06-14 13:09:30 +0200

    Input file formats expanded

--- R/io_funs.R
+++ R/io_funs.R
@@ -83,12 +83,13 @@
 
 # Import from a single file ------
 
-#' Imports tracks from a single text file.
+#' Import tracks from a single formatted text file.
 #'
 #' @description Reads track information from a tab-separated text file of the
 #' following columns named: id, t, x, y and optionally z, containing track id,
 #' time, x, y and optionally z coordinates, respectively.
 #' @details Note: tracks with less than 2 steps are automatically removed.
+#' @return a trax class object.
 #' @param file a path to the file.
 #' @param ... extra arguments passed to \code{\link[readr]{read_tsv}}.
 #' @export
@@ -100,6 +101,96 @@
     coord_tbl <- dplyr::filter(coord_tbl, complete.cases(coord_tbl))
 
     filter_steps(as_trax(coord_tbl), min_steps = 2, return_both = FALSE)
+
+  }
+
+# Import from an extended output of the live imager hardware ------
+
+#' Import tracks from a single unformatted text file.
+#'
+#' @description This the most flexible way of importing tracks from a text file.
+#' It handles most of tabular text formats with named columns. th user is asked
+#' to specify names of columns containing track ID, time and coordinates. It may
+#' work considerably slower than \code{\link{read_trax}} or
+#' \code{\link{read_trax_parts}}.
+#' @details Note: tracks with less than 2 steps are automatically removed. Uses
+#' \code{\link[readr]{read_delim}} under the hood.
+#' @return a trax class object or, if multiple samples defined (sample_id),
+#' a list of trax objects named after the provided sample IDs.
+#' @param file a path to the text file.
+#' @param id_name name of the column storing track IDs.
+#' @param t_name name of the column storing times.
+#' @param x_name name of the column storing x coordinates.
+#' @param y_name name of the column storing y coordinates.
+#' @param z_name name of the column storing z coordinates, optional.
+#' @param sample_name name of the column storing sample IDs, optional.
+#' @param ... extra arguments passed to \code{\link[readr]{read_delim}}.
+#' @export
+
+  read_trax_text <- function(file,
+                             id_name,
+                             t_name,
+                             x_name,
+                             y_name,
+                             z_name = NULL,
+                             sample_name = NULL,
+                             delim = '\t', ...) {
+
+    ## data entry and checks
+
+    text_entry <- readr::read_delim(file = file,
+                                    delim = delim,
+                                    col_names = TRUE,
+                                    show_col_types = FALSE, ...)
+
+    if(any(!c(id_name, t_name, x_name, y_name) %in% names(text_entry))) {
+
+      stop('At least one of id_name, t_name, x_name, y_name
+           does not match column names of the text file.',
+           call. = FALSE)
+
+    }
+
+    if(!is.null(z_name)) {
+
+      if(!z_name %in% names(text_entry)) {
+
+        stop('z_name does not match column names of the text file.')
+
+      }
+
+    }
+
+    if(!is.null(sample_name)) {
+
+      if(!sample_name %in% names(text_entry)) {
+
+        stop('sample_name does not match column names of the text file.')
+
+      }
+
+    }
+
+    ## trax object
+
+    text_entry <- text_entry[c(id_name, t_name, x_name, y_name, z_name)]
+
+    text_entry <- dplyr::filter(text_entry, complete.cases(text_entry))
+
+    if(!is.null(sample_name) & length(unique(text_entry)) > 1) {
+
+      trax_list <- plyr::dlply(text_entry, sample_name, as_trax)
+
+      purrr::map(trax_list,
+                 filter_steps,
+                 min_steps = 2,
+                 return_both = FALSE)
+
+    } else {
+
+      filter_steps(as_trax(text_entry), min_steps = 2, return_both = FALSE)
+
+    }
 
   }
 
